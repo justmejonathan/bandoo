df72cd442307ccb21e776cb51f19b5be1aaa6ab1
Marvin Grocholl <Marvin@Marvins-MBP.fritz.box>
2020-11-13 12:10:10 +0100

    I found out how to receive the form data in the backend

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+#from forms import RegistrationForm
 app = Flask(__name__)
 
 app.config['SECRET KEY'] = '654392748kl3242dfnkl43235hf'
@@ -12,6 +13,17 @@ def index():
 @app.route('/contact')
 def contact():
     return render_template('contact.html', title='Contact')
+
+@app.route('/recommendations', methods=["POST", "GET"])
+def recommendations():
+    if request.method == "POST":
+        artist = request.form["artist"]
+        color = request.form["color"]
+        drink = request.form["drink"]
+        return '''greg{}gre {}terge {}'''.format(artist, color, drink)
+        #redirect(url_for("data", ))
+    else:
+        return render_template('index.html', title='Bandoo')
 
 if __name__=="__main__":
     app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,8 @@
+#from flask_wtf import FlaskForm
+#from wtforms import StringField, SubmitField
+
+#class RegistrationForm(FlaskForm):
+    #username = Stringfield('Username')
+
+#submit = SubmitField('get perfect fit')
+

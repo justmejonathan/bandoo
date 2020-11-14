0259ed6092a43cc2ae4a00baa63c18ec1029fc62
Marvin Grocholl <Marvin@Marvins-MBP.fritz.box>
2020-11-14 23:38:09 +0100

    now I learned how to handle the data, which I get from the frontend and to calculate the basis for the response in the frontend

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, url_for
-#from forms import RegistrationForm
+import sqlite3
 app = Flask(__name__)
 
 app.config['SECRET KEY'] = '654392748kl3242dfnkl43235hf'
 app.static_folder = 'static'
+conn = sqlite3.connect('glasses.db')
+c = conn.cursor()
 
 @app.route('/')
 @app.route('/home')
@@ -20,10 +22,26 @@ def recommendations():
         artist = request.form["artist"]
         color = request.form["color"]
         drink = request.form["drink"]
-        return '''greg{}gre {}terge {}'''.format(artist, color, drink)
-        #redirect(url_for("data", ))
+        gls = int(artist) + int(color) + int(drink)
+        if gls <= 5:
+            c.execute('UPDATE glasses SET counter = counter +1 WHERE id = 1 ')
+            return "I love my Gold Chain"
+        #return '''Your glasses are {}'''.format(gls)
+        elif gls > 5 and gls <=7:
+            c.execute('UPDATE glasses SET counter = counter +1 WHERE id = 2 ')
+            return "My Life is Bling Bling"
+        elif gls > 7 and  gls <=9:
+            c.execute('UPDATE glasses SET counter = counter +1 WHERE id = 3 ')
+            return "I was Born with Style"
+        else: 
+            c.execute('UPDATE glasses SET counter = counter +1 WHERE id = 1 ')
+            return "Beach Vibes Baby"
     else:
         return render_template('index.html', title='Bandoo')
+
+conn.commit()
+
+conn.close()
 
 if __name__=="__main__":
     app.run(debug=True)
